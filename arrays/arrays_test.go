16774d45b3fed31ca232d0cab879062a83305bda
sarajoha <sjcc333@gmail.com>
2024-08-29 14:32:41 +0200

    - add sum arrays test and code to slice arrays

--- arrays/arrays_test.go
+++ arrays/arrays_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("collection of any size", func(t *testing.T) {
@@ -12,5 +15,25 @@ func TestSum(t *testing.T) {
 		if got != want {
 			t.Errorf("got %d want %d given, %v", got, want, numbers)
 		}
+	})
+}
+
+func TestSumAllTails(t *testing.T) {
+
+	checkSums := func(t testing.TB, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2, 3}, []int{5, 6, 7})
+		want := []int{5, 13}
+		checkSums(t, got, want)
+	})
+	t.Run("safely sum some slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{5, 6, 7})
+		want := []int{0, 13}
+		checkSums(t, got, want)
 	})
 }
